d46227181f454c33e4ab499ffbfc4db4531c5800
hotaryuzaki <hotaru_edogawa@yahoo.com>
2022-03-10 23:11:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+let A = [1,1,0,1,0,0,0,1,0,0];
+
+const angkaSama = (arr) => {
+    let prevValue = 0;
+    let countLoop = 0;
+    let countSama = 0;
+
+    arr.map((value, index) => {
+        if (prevValue === value) {
+            countLoop++;
+        }
+        else if (prevValue !== value) {
+            if (countLoop > 1) countSama += countLoop;
+            countLoop = 1;
+        }
+
+        prevValue = value;
+        if (index === arr.length - 1) countSama += countLoop;
+    })
+
+    return countSama;
+}
+
+const coin = angkaSama(A);
+console.log("Adjacency coin is", coin);
