9879f13eb2c6e7260764c09dd927fca64f88ca46
hotaryuzaki <hotaru_edogawa@yahoo.com>
2022-03-10 23:25:16 +0700

    add condition for variable function within range 1 to 1000

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 let A = [1,1,0,1,0,0,0,1,0,0];
 
 const angkaSama = (arr) => {
-    let prevValue = 0;
-    let countLoop = 0;
-    let countSama = 0;
+    if (arr.length > 0 && arr.length < 1001) {
+        let prevValue = 0;
+        let countLoop = 0;
+        let countSama = 0;
 
-    arr.map((value, index) => {
-        if (prevValue === value) {
-            countLoop++;
-        }
-        else if (prevValue !== value) {
-            if (countLoop > 1) countSama += countLoop;
-            countLoop = 1;
-        }
+        arr.map((value, index) => {
+            if (prevValue === value) {
+                countLoop++;
+            }
+            else if (prevValue !== value) {
+                if (countLoop > 1) countSama += countLoop;
+                countLoop = 1;
+            }
 
-        prevValue = value;
-        if (index === arr.length - 1) countSama += countLoop;
-    })
+            prevValue = value;
+            if (index === arr.length - 1) countSama += countLoop;
+        })
 
-    return countSama;
+        return countSama;
+    }
+
+    return false;
 }
 
 const coin = angkaSama(A);
